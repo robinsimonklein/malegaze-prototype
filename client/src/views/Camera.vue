cc90fb43fde6be5331616b75e5f6f7c6d6c90643
Robin Simonklein <rsimonklein@gmail.com>
2020-04-03 17:18:23 +0200

    Add QRCode generation

--- client/src/views/Camera.vue
+++ client/src/views/Camera.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="camera">
     <h1>Camera</h1>
+      <QRCode url="https://www.google.com" />
   </div>
 </template>
 
 
 <script>
     // @ is an alias to /src
+    import QRCode from "../components/QRCode";
     export default {
         name: 'Camera',
         components: {
+            QRCode
         }
     }
 </script>
