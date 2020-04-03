8d65d445fe1f7d5a92c8949cc529a4173586e852
Robin Simonklein <rsimonklein@gmail.com>
2020-04-03 16:59:05 +0200

    Add camera route

---
+++ client/src/views/Camera.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="camera">
+    <h1>Camera</h1>
+  </div>
+</template>
+
+
+<script>
+    // @ is an alias to /src
+    export default {
+        name: 'Camera',
+        components: {
+        }
+    }
+</script>
