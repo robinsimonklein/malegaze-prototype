c05d71fcc4e9800b62c4ce3554d6e1cada4b1bf4
Robin Simonklein <rsimonklein@gmail.com>
2020-04-06 15:52:28 +0200

    Client config for prod

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 let https = false;
 
-if(process.env.VUE_APP_HTTPS === "true"){
+if(process.env.VUE_APP_HTTPS === "true" && process.env.NODE_ENV === 'development'){
     https = {
         key: fs.readFileSync('./cert/robin.local+3-key.pem'),
         cert: fs.readFileSync('./cert/robin.local+3.pem'),
